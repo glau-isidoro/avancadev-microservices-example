f6f376644ae2fb9069ad97fa98529ff80fe9909b
Gláucia Isidoro <glaucia.isidoro@gmail.com>
2020-11-05 22:51:28 -0300

    checkout html example

--- a/a.go
+++ a/a.go
@@ -13,6 +13,6 @@ func main() {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseFiles("templates/home.html"))
+	t := template.Must(template.ParseFiles("templates/checkout.html"))
 	t.Execute(w, "")
 }
