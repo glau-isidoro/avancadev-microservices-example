d7c3f63be375dc578e8b1503b6a241548cb4652e
Gláucia Isidoro <glaucia.isidoro@gmail.com>
2020-11-06 20:58:06 -0300

    get values from form

--- a/a.go
+++ a/a.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
 
 	http.HandleFunc("/", home)
+	http.HandleFunc("/process", process)
 	http.ListenAndServe(":9090", nil)
 
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("templates/checkout.html"))
+	t.Execute(w, "")
+}
+
+func process(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.FormValue("coupon"))
+	log.Println(r.FormValue("cc-number"))
+
 	t := template.Must(template.ParseFiles("templates/checkout.html"))
 	t.Execute(w, "")
 }
