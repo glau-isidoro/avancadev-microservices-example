f1e89ad144597d72469156008f4c50e2d15db088
Gláucia Isidoro <glaucia.isidoro@gmail.com>
2020-11-05 22:15:04 -0300

    server hello world

--- a/a.go
+++ a/a.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
-	fmt.Println("Hello, World")
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":9090", nil)
 
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello, World</h1>")
 }
