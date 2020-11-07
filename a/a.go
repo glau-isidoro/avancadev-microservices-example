6c7d2bc23fc304d0c5049eb1d763cccfdb267ef9
Gláucia Isidoro <glaucia.isidoro@gmail.com>
2020-11-06 21:26:31 -0300

    fatal error

--- a/a.go
+++ a/a.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
+
+type Result struct {
+	Status string
+}
 
 func main() {
 
@@ -16,13 +23,35 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles("templates/checkout.html"))
-	t.Execute(w, "")
+	t.Execute(w, Result{})
 }
 
 func process(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.FormValue("coupon"))
-	log.Println(r.FormValue("cc-number"))
+	result := makeHTTPCall("http://localhost:9191", r.FormValue("coupon"), r.FormValue("cc-number"))
 
 	t := template.Must(template.ParseFiles("templates/checkout.html"))
-	t.Execute(w, "")
+	t.Execute(w, result)
+}
+
+func makeHTTPCall(URLMicroservice string, coupon string, ccNumber string) Result {
+	values := url.Values{}
+	values.Add("coupon", coupon)
+	values.Add("ccNumber", ccNumber)
+
+	res, err := http.PostForm(URLMicroservice, values)
+	if err != nil {
+		log.Fatal("Microservice B out")
+	}
+
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("Error processing result")
+	}
+
+	result := Result{}
+	json.Unmarshal(data, &result)
+
+	return result
 }
